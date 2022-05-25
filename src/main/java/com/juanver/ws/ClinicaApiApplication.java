24c1a71093eac88a5eee968520399e843448c8a4
JuanVera326 <jjvera86@misena.edu.co>
2022-05-25 18:48:20 -0500

    Correciones API Rest

--- src/main/java/com/juanver/ws/ClinicaApiApplication.java
+++ src/main/java/com/juanver/ws/ClinicaApiApplication.java
@@ -2,6 +2,9 @@ package com.juanver.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClinicaApiApplication {
@@ -9,5 +12,13 @@ public class ClinicaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaApiApplication.class, args);
 	}
-
+	/*@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("/*");
+			}
+		};
+	}*/
 }
